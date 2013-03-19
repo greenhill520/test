60d548080e5269140bdde1942ff49ff64196e11b
Wei xinliang <greenhill5200@sina.com>
2013-03-19 13:40:07 +0800

    modify the html and css files, js file

--- default.js
+++ default.js
@@ -42,3 +42,104 @@ function checkPass(){
         return true;
     }
 }
+
+// 2013-3-19 xl 测试表单与文件的函数
+function ShowFileName() {
+    var file;
+    for( var i = 0; i < document.getElementById("file").files.length; i++ ) {
+        file = document.getElementById("file").files[i];
+        alert(file.name);
+    }
+}
+
+function ShowFileType() {
+    var file;
+    file = document.getElementById("file").files[0];
+    var size = document.getElementById("size");
+    size.innerHTML = file.size;
+    var type = document.getElementById("type");
+    type.innerHTML = file.type;
+}
+
+function FileUpload() {
+    var file;
+    for( var i = 0; i < document.getElementById("file").files.length; i++ ){
+        file = document.getElementById("file").files[i];
+        if( !/image\/\w+/.test(file.type) ) {
+            alert(file.name + "文件不是图像文件！");
+            break;
+        }
+        else {
+            alert(file.name + "文件已上传！");
+        }
+    }
+}
+
+var result = document.getElementById("result");
+var file2 = document.getElementById("file2");
+if( typeof FileReader == 'undefined' ) {
+    result.innerHTML = "<p>抱歉，你的浏览器不支持FileReader！</p>";
+    file2.setAttribute('disabled', 'disabled');
+}
+function readAsDataURL() {
+    var file2 = document.getElementById("file2").files[0];
+    if( !/image\/\w+/.test(file2.type) ) {
+        alert("请确保文件图像类型！");
+        return false;
+    }
+    var reader = new FileReader();
+    reader.readAsDataURL(file2);
+    reader.onload = function()
+    {
+        var result = document.getElementById("result");
+        result.innerHTML = '<img src="'+ this.result + '" alt=""/>';
+    };
+    return true;
+}
+function readAsBinaryString() {
+    var file2 = document.getElementById("file2").files[0];
+    var reader = new FileReader();
+    reader.readAsBinaryString(file2);
+    reader.onload = function()
+    {
+        var result = document.getElementById("result");
+        result.innerHTML = this.result;
+    };
+}
+function readAsText(){
+    var file2 = document.getElementById("file2").files[0];
+    var reader = new FileReader();
+    reader.readAsText(file2);
+    reader.onload = function()
+    {
+        var result = document.getElementById("result");
+        result.innerHTML = this.result;
+    };
+}
+
+function saveStorage(id) {
+    var data = document.getElementById(id).value;
+    var time = new Date().getTime();
+    localStorage.setItem(time, data);
+    alert("数据已保存！");
+    loadStorage('msg');
+}
+function loadStorage(id) {
+    var result = '<table border="1">';
+    for( var i = 0; i < localStorage.length; i++ ){
+        var key = localStorage.key(i);
+        var value = localStorage.getItem(key);
+        var date = new Date();
+        date.setTime(key);
+        var datestr = date.toGMTString();
+        result += '<tr><td>' + value + '</td><td>' + datestr + '</td></tr>';
+    }
+    result += '</table>';
+    var target = document.getElementById(id);
+    target.innerHTML = result;
+}
+function clearStorage() {
+    localStorage.clear();
+    alert("全部数据被清除！");
+    loadStorage('msg');
+}
