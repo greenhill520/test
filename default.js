deaf64d24008f63ab687572220dd808faba8c6e6
Wei xinliang <greenhill5200@sina.com>
2013-03-26 14:17:27 +0800

    modify the html and css files, js file and add txt file

--- default.js
+++ default.js
@@ -138,8 +138,114 @@ function loadStorage(id) {
     var target = document.getElementById(id);
     target.innerHTML = result;
 }
-function clearStorage() {
+function clearStorage(id) {
     localStorage.clear();
     alert("全部数据被清除！");
-    loadStorage('msg');
+    loadStorage(id);
+}
+
+// 2013-3-19 xl 增加获取地理位置信息
+/*
+function showObject(obj,k) {
+    if(!obj) {
+        return;
+    }
+    for(var i in obj) {
+        if(typeof (obj[i]) != "object" || obj[i] == null) {
+            for(var j = 0; j < k; j++ ){
+                document.write("&nbsp;&nbsp;&nbsp;&nbsp;");
+            }
+            document.write( i + " : " + obj[i] + "<br/>");
+        }
+        else {
+            document.write( i + " : " + "<br/>");
+            showObject(obj[i],k+1);
+        }
+    }
+}
+function get_location() {
+    if ( navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(show_map, handle_error,
+            {enableHighAccuracy:true, maximumAge:1000});
+    }
+    else {
+        alert("你的浏览器不支持HTML 5来获取地理位置信息！");
+    }
+}
+function handle_error(err) {
+    switch ( err.code ){
+        case 1:
+            alert("位置服务被拒绝！");
+            break;
+        case 2:
+            alert("暂时获取不到位置信息！");
+            break;
+        case 3:
+            alert("获取信息超时！");
+            break;
+        default :
+            alert("未知错误！");
+            break;
+    }
+}
+function show_map(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    showObject(position,0);
+}
+get_location();
+*/
+function init() {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        var coords = position.coords;
+        var latlng = new google.maps.LatLng(coords.latitude, coords.longitude);
+        var myOptions = {
+            zoom: 14,
+            center: latlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map1;
+        map1 = new google.maps.Map(document.getElementById("map"), myOptions);
+        var marker = new google.maps.Marker({
+            position: latlng,
+            map: map1
+        });
+        var infowindow = new google.maps.InfoWindow({
+            content: "当前位置！"
+        });
+        infowindow.open(map1, marker);
+    });
+}
+
+// 2013-03-26 xl
+//增加测试时候有不合法的词语出现
+function CheckWrong() {
+    var arr = readFile("test.txt");
+    var tf = 0;
+    var storeW = "";
+    var str = document.getElementById("checkwrong").value;
+    for(var i = 0; i < arr.length; i++ ) {
+        if(str.indexOf(arr[i]) != -1) {
+            tf = 1;
+            storeW += arr[i];
+            storeW += ", ";
+        }
+    }
+    if( tf == 1) {
+        alert("你输入的内容有不和谐的内容（"+storeW+"）！请检查！");
+    }
+    else {
+        alert("恭喜，你输入的内容没有不和谐的内容！");
+    }
+}
+function readFile(filename){
+    var fso = new ActiveXObject("Scripting.FileSystemObject");
+    var f = fso.OpenTextFile(filename,1);
+    var s = "";
+    var dataArray = Array();
+    while (!f.AtEndOfStream)
+        s = f.ReadLine();
+        dataArray.push(s);
+    f.Close();
+    return dataArray;
 }
